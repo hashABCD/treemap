af03bd5c8204316ace2f42c2910bde9fd3f26303
unknown <abhijith.chandradas@gmail.com>
2021-06-17 12:31:54 +0100

    finvize theme applied

--- treemap.py
+++ treemap.py
@@ -10,15 +10,18 @@ df.Mcap.fillna('NA', inplace=True)
 #new colum adjusted marketcap
 df['adjmcap']=df.Mcap.apply(lambda x:1 if x=='NA' else 1 if x<1 else x)
 
+#Finviz Colorscheme
+df['cat_change']=df.Change.apply(lambda x: '3pl' if x>3 else '2pl' if x>2 else '1pl' if x>1 else 'zero' if x>-1 else '1min'if x>-2 else '2min' if x>-3 else '3min' )
+disc_map={'(?)':'black', '3min': '#FF0000','2min': "#CD5C5C",'1min':"#8B0000", 
+          'zero':"#696969", '1pl':"#556B2F",  '2pl':"#228B22",'3pl':"#32CD32"}
+
 #adding super container for entire treemap
 df['perf']='Market Performance'
 
 fig = px.treemap(df,path=['perf','Sector', 'Industry','Symbol'],
                  values='adjmcap',
-                 names='Change',
-                 color='Change',
-                color_continuous_scale='RdYlGn',
-                color_continuous_midpoint=0)    #np.average(df['Change'], weights=df['adjmcap'])
+                 color='cat_change',
+                color_discrete_map=disc_map)   
 
 #save image
 fig.write_image("fig1.png", height=800, width=1200)
